29707b7d7234df3cc7aca74ad77d348763e32d23
daniloviic <danilovic.milos96@gmail.com>
2017-03-21 14:39:15 +0100

    Uvedena klasa biblioteka koja implementira interfejs BibliotekaInterfejs

--- Biblioteka2017/src/biblioteka/Biblioteka.java
+++ Biblioteka2017/src/biblioteka/Biblioteka.java
@@ -7,28 +7,30 @@ import biblioteka.interfejs.Bibliotekainterfejs;
 public class Biblioteka implements Bibliotekainterfejs {
 	
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
-	@Override
+	
 	public void dodajKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
-
+		knjige.add(k);
 	}
 
-	@Override
+	
 	public void obirisKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
-
+		knjige.remove(k);
 	}
 
-	@Override
+	
 	public LinkedList<Knjiga> vratiSveKnjige() {
-		// TODO Auto-generated method stub
-		return null;
+		return knjige;
 	}
 
-	@Override
+	
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor a, long isbn, String naslov, String izdavac) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		for (int i = 0; i < knjige.size(); i++) {
+			if(knjige.get(i).getNaslov().contains(naslov))
+				rezultat.add(knjige.get(i));
+		}
+		return rezultat;
 	}
 
 }
